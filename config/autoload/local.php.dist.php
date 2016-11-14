30b15ec4578279ba60cf2a1ed0681d2f6f028b99
AleksandraSp <aleksandranspasojevic@gmail.com>
2016-11-14 10:17:15 +0100

    added local.php.dist config file missing

---
+++ config/autoload/local.php.dist.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Local Configuration Override
+ *
+ * This configuration override file is for overriding environment-specific and
+ * security-sensitive configuration information. Copy this file without the
+ * .dist extension at the end and populate values as needed.
+ */
+return array(
+     'db' => array(
+         'username' => 'username', 
+         'password' => 'password',
+     ),
+ );
